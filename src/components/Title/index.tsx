c92dc5241bae4c75f09240a850f3d77fade71f74
Lluis Arevalo Salom <lluisarevalo@lluiss-mbp.lan>
2025-03-02 15:47:08 +0100

    Config style components and create Title Component

---
+++ src/components/Title/index.tsx
@@ -0,0 +1,6 @@
+'use client';
+import { TitleComponent } from "./styles";
+
+export default function Title() {
+  return <TitleComponent>Michael Myers header</TitleComponent>
+}
---
+++ src/components/Title/styles.tsx
@@ -0,0 +1,7 @@
+import Styled from 'styled-components';
+
+export const TitleComponent = Styled.h1`
+  font-size: 3.2rem;
+  padding: 1.6rem 0;
+  text-align: center;
+`;
